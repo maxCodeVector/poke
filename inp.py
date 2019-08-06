31799c3f2761ec890bd02d10bbe6e0a5f20af62f
maxCodeVector <11610303@mail.sustech.edu.cn>
2019-08-07 00:07:05 +0800

    go run 0.07s

--- inp.py
+++
@@ -1,7 +0,0 @@
-import json
-
-
-def loadFont(path):
-    f = open(path, encoding='utf-8')
-    setting = json.load(f)
-    return setting
--- poke.go
+++ poke.go
@@ -5,19 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
-	"os"
 	"sort"
 )
-
-type Game struct {
-	alice  string
-	bob    string
-	result int
-}
-
-type Record struct {
-	matches *[]Game
-}
 
 const (
 	NoneCard       = 0
@@ -56,72 +45,17 @@ var SPECIAL_STAIGHT = []int{2, 3, 4, 5, 14}
 var CARD_BIT = 16
 var CARD_A_PART = CARD_TABLE["A"] * int(math.Pow(16, 4))
 
-type Cards struct {
-	cardMap map[int]int
-	max int
-	min int
-	isFlush bool
-	cardType int
-	score int
+type Game struct {
+	Alice  string
+	Bob    string
+	Result int
 }
 
-func(c Cards) NewCards(cards string)  {
-	c.max = 0
-	c.min = 100
-	c.isFlush = true
-	c.score = 0
-	preColor := CARD_TABLE[string(cards[1])]
-	for i := 0; i < len(cards); i+=2 {
-		cardNum := CARD_TABLE[string(cards[i])]
-		cardColor := CARD_TABLE[string(cards[i + 1])]
-
-		c.max = max2int(c.max, cardNum)
-		c.min =min2int(c.min, cardNum)
-		_, ok := c.cardMap[cardNum]
-		if !ok {
-			c.cardMap[cardNum] = 1
-		}else {
-			c.cardMap[cardNum] += 1
-		}
-
-		if cardColor != preColor{
-			c.isFlush = false
-		}
-	}
+type Record struct {
+	Matches *[]Game
 }
 
-
-func (c Cards) iniScoreInEqualCase() {
-	score := 0
-	type kv struct {
-		Key   int
-		Value int
-	}
-
-	var ss []kv
-	for k, v := range c.cardMap {
-		ss = append(ss, kv{k, v})
-	}
-
-	sort.Slice(ss, func(i, j int) bool {
-		if ss[i].Value > ss[j].Value{
-			return true
-		}else if ss[i].Value == ss[j].Value && ss[i].Key > ss[i].Key{
-			return true
-		}
-		return false
-	})
-	for _, card := range ss{
-		for x:=0;x<card.Value;x++{
-			score = score * CARD_BIT + card.Key
-		}
-	}
-
-	c.score += score
-}
-
-
-func loadFont(fileName string) *[] Game {
+func loadJsonFile(fileName string) *[] Game {
 
 	data, err := ioutil.ReadFile(fileName)
 	if err != nil {
@@ -132,24 +66,88 @@ func loadFont(fileName string) *[] Game {
 	if err != nil {
 		return nil
 	}
-	return record.matches
+	return record.Matches
 
+}
+
+type Cards struct {
+	cardMap  map[int]int
+	max      int
+	min      int
+	isFlush  bool
+	cardType int
+	score    int
+}
+
+func NewCards(c *Cards, cards string) {
+	c.max = 0
+	c.min = 100
+	c.isFlush = true
+	c.score = 0
+	c.cardMap = make(map[int]int)
+	preColor := CARD_TABLE[string(cards[1])]
+	for i := 0; i < len(cards); i += 2 {
+		cardNum := CARD_TABLE[string(cards[i])]
+		cardColor := CARD_TABLE[string(cards[i+1])]
+
+		c.max = max2int(c.max, cardNum)
+		c.min = min2int(c.min, cardNum)
+		_, ok := c.cardMap[cardNum]
+		if !ok {
+			c.cardMap[cardNum] = 1
+		} else {
+			c.cardMap[cardNum] += 1
+		}
+
+		if c.isFlush && cardColor != preColor {
+			c.isFlush = false
+		}
+	}
+}
+
+type kv struct {
+	Key   int
+	Value int
+}
+
+func (c *Cards) iniScoreInEqualCase() {
+	score := 0
+
+	var ss []kv
+	for k, v := range c.cardMap {
+		ss = append(ss, kv{k, v})
+	}
+
+	sort.Slice(ss, func(i, j int) bool {
+		if ss[i].Value > ss[j].Value {
+			return true
+		} else if ss[i].Value == ss[j].Value && ss[i].Key > ss[j].Key {
+			return true
+		}
+		return false
+	})
+	for _, card := range ss {
+		for x := 0; x < card.Value; x++ {
+			score = score*CARD_BIT + card.Key
+		}
+	}
+
+	c.score += score
 }
 
 type Comparator struct {
 }
 
 type BaseComparator interface {
-	compare(cards1, cards2 Cards) int
+	compare(cards1, cards2 *Cards) int
 }
 
-
-func (comp Comparator) compare(cards1, cards2 Cards) int{
-	comp.judge_cardType(cards1)
-	comp.judge_cardType(cards2)
+func (comp *Comparator) compare(cards1, cards2 *Cards) int {
+	comp.judgeCardType(cards1)
+	comp.judgeCardType(cards2)
 	if cards1.cardType > cards2.cardType {
 		return 1
-	}else if cards1.cardType < cards2.cardType {
+	} else if cards1.cardType < cards2.cardType {
 		return 2
 	} else {
 		cards1.iniScoreInEqualCase()
@@ -164,18 +162,18 @@ func (comp Comparator) compare(cards1, cards2 Cards) int{
 	}
 }
 
-func (comp Comparator)judge_cardType(cards Cards) {
+func (comp *Comparator) judgeCardType(cards *Cards) {
 	if len(cards.cardMap) == 5 {
 		comp.judgeStraightType(cards)
-	}else if len(cards.cardMap) == 4{
+	} else if len(cards.cardMap) == 4 {
 		cards.cardType = Double_OneCard
-	}else if len(cards.cardMap) == 3{
+	} else if len(cards.cardMap) == 3 {
 		if maxValueOfMap(&cards.cardMap) == 3 {
 			cards.cardType = ThreeCard
 		} else {
 			cards.cardType = Double_TwoCard
 		}
-	}else if len(cards.cardMap) == 2 {
+	} else if len(cards.cardMap) == 2 {
 		if maxValueOfMap(&cards.cardMap) == 4 {
 			cards.cardType = FourCard
 		} else {
@@ -188,14 +186,14 @@ func (comp Comparator)judge_cardType(cards Cards) {
 }
 
 func max2int(a, b int) int {
-	if a > b{
+	if a > b {
 		return a
 	}
 	return b
 }
 
 func min2int(a, b int) int {
-	if a < b{
+	if a < b {
 		return a
 	}
 	return b
@@ -203,26 +201,25 @@ func min2int(a, b int) int {
 
 func maxValueOfMap(m *map[int]int) int {
 	maxV := 0
-	for _, v := range *m{
+	for _, v := range *m {
 		maxV = max2int(maxV, v)
 	}
 	return maxV
 }
 
-
-func (comp Comparator) judgeStraightType(cards Cards) {
-	if comp.baseJudgeStaight(cards) {
+func (comp *Comparator) judgeStraightType(cards *Cards) {
+	if !comp.baseJudgeStraight(cards) {
 		cards.cardType = HighCard
-	}else if !cards.isFlush {
+	} else if !cards.isFlush {
 		cards.cardType = StraightCard
-	}else if int(cards.max) == CARD_TABLE["A"] {
+	} else if cards.max == CARD_TABLE["A"] {
 		cards.cardType = RoyalFlush
-	}else {
+	} else {
 		cards.cardType = StraightFlush
 	}
 }
 
-func( comp Comparator) baseJudgeStaight(cards Cards)bool{
+func (comp *Comparator) baseJudgeStraight(cards *Cards) bool {
 	if cards.max-cards.min == 4 {
 		return true
 	}
@@ -237,7 +234,7 @@ func( comp Comparator) baseJudgeStaight(cards Cards)bool{
 func isKeysInKeys(l *[]int, m *map[int]int) bool {
 	for _, e := range *l {
 		_, ok := (*m)[e]
-		if !ok{
+		if !ok {
 			return false
 		}
 	}
@@ -246,22 +243,47 @@ func isKeysInKeys(l *[]int, m *map[int]int) bool {
 }
 
 func main() {
-	t := loadFont("result.json")
-	var comparator BaseComparator
-	comparator = Comparator{}
+	t := loadJsonFile("test_file/result.json")
+	//var comparator BaseComparator
+	comparator := Comparator{}
+	const threadNum = 4
 
-	for _, game := range *t {
-		var aliceCard Cards
-		var bobCard Cards
-		aliceCard.NewCards(game.alice)
-		bobCard.NewCards(game.bob)
-		res := comparator.compare(aliceCard, bobCard)
-		if res != game.result{
-			os.Exit(-1)
+	var aliceCard Cards
+	var bobCard Cards
+	for _, game := range (*t) {
+		NewCards(&aliceCard, game.Alice)
+		NewCards(&bobCard, game.Bob)
+		res := comparator.compare(&aliceCard, &bobCard)
+		if res != game.Result {
+			panic("I am panic")
 		}
-
 	}
 
-	fmt.Print("Are you happy?\n")
+	//runtime.GOMAXPROCS(threadNum)
+	//
+	//var flag [threadNum]chan int
+	//for x := 0; x < threadNum; x++ {
+	//	start := x * len(*t) / threadNum
+	//	end := max2int((x+1)*len(*t)/threadNum, len(*t))
+	//	flag[x] = make(chan int)
+	//	go thread(t, &comparator, start, end, flag[x])
+	//}
+	//for x := 0; x < threadNum; x++ {
+	//	<-flag[x]
+	//}
+	fmt.Printf("%d Thread, are you happy?\n", threadNum)
+}
 
+func thread(t *[]Game, comparator *Comparator, start int, end int, flag chan int) {
+	var aliceCard Cards
+	var bobCard Cards
+	for _, game := range (*t)[start:end] {
+		NewCards(&aliceCard, game.Alice)
+		NewCards(&bobCard, game.Bob)
+		res := comparator.compare(&aliceCard, &bobCard)
+		if res != game.Result {
+			panic("I am panic")
+		}
+	}
+	flag <- 1
 }
